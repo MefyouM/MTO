aede1a5d31b468ea4a1a5e8189f2cb152f293086
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-03-14 18:30:32 +0100

    Update main.py
    Testowanie wyświetlania efektów w pliku output

--- lab3/main.py
+++ lab3/main.py
@@ -5,10 +5,11 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    #format_string.swapcase()
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                print("XXX",end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
