caebe62be439e42e13d35f549cd2ac1c35491a52
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-03-14 18:36:29 +0100

    Update main.py
    Dodanie zmiany rozmiaru liter. Spowodowało to zamianę rozmiaru wszytskich liter w pliku wyjściowym

--- lab3/main.py
+++ lab3/main.py
@@ -4,8 +4,8 @@ import sys
 
 def my_printf(format_string,param):
     #print(format_string)
+    format_string = format_string.swapcase()
     shouldDo=True
-    #format_string.swapcase()
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
