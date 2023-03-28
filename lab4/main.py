7e5cac4a56f00c990b3d20108294eef527b84f06
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-03-28 18:33:14 +0200

    Update main.py
    Wprowadzenie odwracania parametru

--- lab4/main.py
+++ lab4/main.py
@@ -3,12 +3,11 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                print(param,end="")
+                print(param[::-1],end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
