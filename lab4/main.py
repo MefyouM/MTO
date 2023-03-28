b961915541d14747b9d6adde042acfb29181b006
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-03-28 18:48:14 +0200

    Update main.py
    Odwracanie tylko liczb. Nie przepisuje ciągów literowych. Zakończony program.

--- lab4/main.py
+++ lab4/main.py
@@ -7,8 +7,9 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                print(param[::-1],end="")
-                shouldDo=False
+            	if (param.isnumeric()):
+                	print(param[::-1],end="")
+            	shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
