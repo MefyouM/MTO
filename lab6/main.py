0bc9376ef0d3f8a0879c7b9fc8876eddd87e9439
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-04-18 18:02:43 +0200

    Update main.py
    Edycja programu przykładowego tak by wykonywał na wczytanych liczba oczekiwane obliczenia czyli y = ((x*9)+1)%10

--- lab6/main.py
+++ lab6/main.py
@@ -5,10 +5,19 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    morf = ""
+    for i in range(len(param)):
+        liczba = int(param[i])
+        liczba = liczba*9
+        liczba = liczba+1
+        liczba = liczba%10
+        cyfra = str(liczba)
+        morf += cyfra
+    morf = str(morf)
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'g':
+                print(morf,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
