f1d10eb1076c5997f8d1979687c6636eb82c90a3
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-05-16 18:29:27 +0200

    Update main.py
    Napisanie funkcjonalności pliku, przekształcanie znaków w odpowiedni sposób. brak jeszcze dopisywania zer (lub znaków o) w celu dopełnienia do rządanej ilości znaków

--- lab8/main.py
+++ lab8/main.py
@@ -5,15 +5,55 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    sign = ""
+    morf = ""
+    final = ""
+    indeks = 0
+    c = 0
+    for i in range(len(param)):
+        sign = param[i]
+        if sign == "0":
+            sign = "o"
+        elif sign == "A":
+            sign = "G"
+        elif sign == "B":
+            sign = "H"
+        elif sign == "C":
+            sign = "I"
+        elif sign == "D":
+            sign = "J"
+        elif sign == "E":
+            sign = "K"
+        elif sign == "F":
+            sign = "L"
+        elif sign == "a":
+            sign = "g"
+        elif sign == "b":
+            sign = "h"
+        elif sign == "c":
+            sign = "i"
+        elif sign == "d":
+            sign = "j"
+        elif sign == "e":
+            sign = "k"
+        elif sign == "f":
+            sign = "l"
+        morf += sign
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param.swapcase(),end="")
+            if format_string[idx] == '#' and format_string[idx+1] == '.':
+                indeks = format_string[idx:].index("j")
+                final = format_string[idx+2:idx+indeks]
+                print(morf[:int(final)],end="")
                 shouldDo=False
+                c = len(final)+1
             else:
                 print(format_string[idx],end="")
         else:
-            shouldDo=True
+            if c == 0:
+                shouldDo=True
+            else:
+                c = c-1
     print("")
 
 data=sys.stdin.readlines()
