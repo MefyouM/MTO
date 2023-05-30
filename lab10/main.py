3091800eff640a89a380d1de6e76fcd7aad6881f
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-05-30 18:04:01 +0200

    Update main.py
    Operacje wykonują się teraz tylko gdy parametr zostanie określony przez #a a nie w każdym przypadku

--- lab10/main.py
+++ lab10/main.py
@@ -4,15 +4,15 @@ import sys
 
 def my_printf(format_string,param):
     #print(format_string)
-    # F=int((O*2)/N)
     shouldDo=True
-    paramInt = int(param)
-    N = len(str(paramInt))
-    O = paramInt
-    F = int((O*2)/N)
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'a':
+                paramF = float(param)
+                paramInt = int(paramF)
+                N = len(str(paramInt))
+                O = paramInt
+                F = int((O*2)/N)
                 print(F,end="")
                 shouldDo=False
             else:
@@ -21,7 +21,7 @@ def my_printf(format_string,param):
             shouldDo=True
     print("")
     #print("Liczba cyfr : "+str(N))
-    
+
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
