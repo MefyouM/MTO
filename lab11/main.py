d95b2b49dc9698a6cf93508530a353df392d0c7f
MefyouM <57044893+MefyouM@users.noreply.github.com>
2023-06-06 17:47:21 +0200

    Update main.py
    Działający program przetwarzający liczbę %d w żądany sposób

--- lab11/main.py
+++ lab11/main.py
@@ -5,24 +5,28 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    paramOut = ""
+    pattern = "abcdefghij"
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'a':
-                paramF = float(param)
-                paramInt = int(paramF)
-                N = len(str(paramInt))
-                O = paramInt
-                if paramInt < 0:
-                    N = N-1
-                F = int((O*2)/N)
-                print(F,end="")
+            if format_string[idx] == '#' and format_string[idx+1] == 'b':
+                paramInt = int(param)
+                paramBin = bin(paramInt)[2:]
+                paramRev = paramBin[::-1]
+                paramStr = str(paramRev)
+                for i in range(len(paramStr)):
+                    if paramStr[i] == "0":
+                        paramOut += "0"
+                    else:
+                        paramOut += pattern[i%10]
+                paramFinal = paramOut[::-1]
+                print(paramFinal,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
             shouldDo=True
     print("")
-    #print("Liczba cyfr : "+str(N))
 
 data=sys.stdin.readlines()
 
